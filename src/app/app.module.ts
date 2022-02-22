2d68cd6cb5f22264e35cf4a7440f61913bf01bf2
OAnisimov <OAnisimov@it-one.ru>
2022-02-22 16:32:22 +0300

    2 lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {NotesComponent} from './notes/notes.component';
+import {HttpClientModule} from "@angular/common/http";
+import {NotesServiceService} from "./notes-service.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    NotesServiceService,
+  ],
+  bootstrap: [
+    AppComponent,
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
